a433a0d9762a294811ef5df3784f3b553e4627cd
Hagminism <iilhm110629@gmail.com>
2026-04-07 22:48:22 +0900

    feat: 카카오 로그인 기능 추가 및 AuthRepository 인터페이스 수정
    - `auth_repository.dart`: 카카오 로그인을 위한 `signInWithKakao` 메서드 정의. `getCurrentUser` 메서드의 반환 타입을 `Future<User?>`에서 `User?`로 변경하여 동기적으로 반환하도록 수정.
    - `auth_repository_impl.dart`: `kakao_flutter_sdk`를 사용하여 `signInWithKakao` 기능 구현. 카카오톡 설치 여부에 따라 카카오톡 또는 카카오계정 로그인 시도 로직 추가.
    - `auth_repository_impl.dart`: 의존성 주입을 위한 `UserApi` 필드 추가 및 `getCurrentUser` 구현 방식 수정.

--- lib/core/domain/repository/auth_repository.dart
+++ lib/core/domain/repository/auth_repository.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract interface class AuthRepository {
+  // 구글
   Future<void> signInWithGoogle();
   Future<void> signOut();
   Future<void> deleteAccount();
-  Future<User?> getCurrentUser();
+  User? getCurrentUser();
   Stream<User?> authStateChanges();
+
+  // 카카오
+  Future<void> signInWithKakao();
 }
