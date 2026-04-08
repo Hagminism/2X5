21b92dd9914127a53094bd31691c67ad7ba840b3
Hagminism <iilhm110629@gmail.com>
2026-04-08 16:35:56 +0900

    refactor: 카카오 로그인 및 계정 탈퇴 로직 고도화
    - auth_repository.dart: `signInWithKakao` 메서드 위치를 조정하고 주석을 정리하여 가독성 개선.
    - auth_repository_impl.dart: `signInWithKakao`를 `OAuthProvider("oidc.kakao")`를 사용하여 구현.
    - auth_repository_impl.dart: `deleteAccount` 메서드 내에 로그인 제공자(Google, Kakao)별 재인증 로직 추가.
    - auth_repository_impl.dart: 계정 탈퇴 시 현재 사용자 존재 여부를 확인하고, 지원하지 않는 인증 수단에 대한 예외 처리 로직 추가.

--- lib/core/domain/repository/auth_repository.dart
+++ lib/core/domain/repository/auth_repository.dart
@@ -1,13 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract interface class AuthRepository {
-  // 구글
   Future<void> signInWithGoogle();
+  Future<void> signInWithKakao();
   Future<void> signOut();
   Future<void> deleteAccount();
   User? getCurrentUser();
   Stream<User?> authStateChanges();
-
-  // 카카오
-  Future<void> signInWithKakao();
 }
