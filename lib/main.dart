620df48729ba6edc9394b34526a5b9e9713ba866
Hagminism <iilhm110629@gmail.com>
2026-04-13 23:28:27 +0900

    feat: 환경 변수 관리(dotenv) 초기화 코드 추가
    - main.dart: 앱 진입점(`main`)에 `dotenv.load()` 호출을 추가하여 `.env` 파일의 환경 변수를 로드하도록 설정.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:capstone_2026/di/di_setup.dart';
 import 'package:capstone_2026/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_common.dart';
 
@@ -26,7 +27,10 @@ Future<void> main() async {
     nativeAppKey: kakaoNativeAppKey,
   );
 
+  await dotenv.load(fileName: '.env');
+
   diSetup();
+
   runApp(const App());
 }
 
