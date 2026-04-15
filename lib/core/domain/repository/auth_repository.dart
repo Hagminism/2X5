1cdc2ff5e0b17581987c8c2d1d1b6e602ca5a52b
Hagminism <iilhm110629@gmail.com>
2026-04-15 18:29:53 +0900

    feat: 이메일 로그인 및 회원가입 인터페이스 분리
    - `auth_repository.dart`: `signInWithEmail` 메서드에서 `name` 파라미터를 제거하고, 회원가입을 위한 `signUpWithEmail` 메서드를 별도로 추가하여 역할 분리.

--- lib/core/domain/repository/auth_repository.dart
+++ lib/core/domain/repository/auth_repository.dart
@@ -3,7 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 abstract interface class AuthRepository {
   // 구글
   Future<void> signInWithGoogle();
-  Future<void> signInWithEmail(String email, String password, String name);
+  Future<void> signInWithEmail(String email, String password);
+  Future<void> signUpWithEmail(String email, String password, String name);
 
   // 카카오
   Future<void> signInWithKakao();
