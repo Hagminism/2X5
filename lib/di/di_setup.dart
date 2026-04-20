785544fadd227e22404417c7e10afdb3e9b09a02
Hagminism <iilhm110629@gmail.com>
2026-04-20 20:07:50 +0900

    feat: 고객 온보딩(OnBoardingCustomer) ViewModel 의존성 주입 설정 추가
    - di_setup.dart: `OnBoardingCustomerViewModel`을 Factory로 등록하고, `AuthRepository` 및 `UserType.customer`를 주입하도록 설정.

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,9 +1,11 @@
 import 'package:app_links/app_links.dart';
 import 'package:capstone_2026/core/data/repository/auth/auth_repository_impl.dart';
+import 'package:capstone_2026/core/domain/model/enum/user_type.dart';
 import 'package:capstone_2026/core/domain/repository/auth/auth_repository.dart';
 import 'package:capstone_2026/feature/find_password/presentation/screen/find_password_view_model.dart';
 import 'package:capstone_2026/feature/my_page/account_settings/presentation/screen/account_setting_view_model.dart';
 import 'package:capstone_2026/feature/my_page/settings/presentation/screen/my_page_view_model.dart';
+import 'package:capstone_2026/feature/on_boarding_customer/presentation/screen/on_boarding_customer_view_model.dart';
 import 'package:capstone_2026/feature/select_auth_provider/presentation/screen/select_auth_provider_view_model.dart';
 import 'package:capstone_2026/feature/sign_in/presentation/screen/sign_in_view_model.dart';
 import 'package:capstone_2026/feature/sign_up_customer/presentation/screen/sign_up_customer_view_model.dart';
@@ -61,5 +63,11 @@ void diSetup() {
   );
   getIt.registerFactory<MyPageViewModel>(
     () => MyPageViewModel(authRepository: getIt<AuthRepository>()),
+  );
+  getIt.registerFactory<OnBoardingCustomerViewModel>(
+    () => OnBoardingCustomerViewModel(
+      authRepository: getIt<AuthRepository>(),
+      userType: UserType.customer,
+    ),
   );
 }
