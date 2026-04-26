07cff1b9833e65b059eae3224b23df575f21416f
Seongmin Oh <seongmin0101@hansung.ac.kr>
2026-04-27 02:16:43 +0900

    feat: Supabase 연동을 통한 검색 구현

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -27,6 +27,7 @@ import 'package:capstone_2026/feature/sign_up_partner/presentation/sign_up_partn
 import 'package:capstone_2026/feature/sign_up_customer/presentation/screen/sign_up_customer_screen_root.dart';
 import 'package:capstone_2026/feature/sign_up_customer/presentation/screen/sign_up_customer_view_model.dart';
 import 'package:capstone_2026/feature/sign_up_type/presentation/screen/sign_up_type_screen_root.dart';
+import 'package:capstone_2026/feature/search/presentation/screen/search_screen.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 
@@ -90,6 +91,11 @@ final router = GoRouter(
               path: Routes.home,
               builder: (context, state) => const HomeScreen(),
               routes: [
+                GoRoute(
+                  parentNavigatorKey: _rootNavigatorKey,
+                  path: Routes.search,
+                  builder: (context, state) => const SearchScreen(),
+                ),
                 GoRoute(
                   parentNavigatorKey: _rootNavigatorKey,
                   path: Routes.homeStoreDetail,
