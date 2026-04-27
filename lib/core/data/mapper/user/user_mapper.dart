d274c7afa4ca0d7308deb81d9b2dd9a21db684ab
Hagminism <iilhm110629@gmail.com>
2026-04-27 14:52:36 +0900

    feat: User 모델과 DTO 간의 매퍼(Mapper) 구현
    - lib/core/data/mapper/user/user_mapper.dart: `UserDto`와 `User` 도메인 모델 간의 상호 변환을 위한 확장 메서드(`toModel`, `toDto`) 정의.
    - 데이터 변환: 문자열 형식의 `userType`을 `UserType` 열거형으로 변환하는 매핑 로직 포함.

---
+++ lib/core/data/mapper/user/user_mapper.dart
@@ -0,0 +1,29 @@
+import 'package:capstone_2026/core/data/dto/user/user_dto.dart';
+import 'package:capstone_2026/core/domain/model/enum/user_type.dart';
+import 'package:capstone_2026/core/domain/model/user/user.dart';
+
+extension UserDtoMapper on UserDto {
+  User toModel() {
+    return User(
+      id: '',
+      name: name!,
+      userType: (userType == 'customer') ? UserType.customer : UserType.partner,
+      email: email!,
+      phone: phone!,
+      imageUrl: imageUrl!,
+    );
+  }
+}
+
+extension UserToDtoMapper on User {
+  UserDto toDto() {
+    return UserDto(
+      id: id,
+      name: name,
+      userType: userType.name,
+      email: email,
+      phone: phone,
+      imageUrl: imageUrl,
+    );
+  }
+}
