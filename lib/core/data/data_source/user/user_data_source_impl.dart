d06b10857e5d98529cb28ced5dc3c1eb0febe0d3
Hagminism <iilhm110629@gmail.com>
2026-04-27 15:09:43 +0900

    feat: UserDataSourceImpl 내 createUser 메서드 구현
    - user_data_source_impl.dart: `SupabaseClient`를 생성자를 통해 주입받도록 변경 및 필드 추가.
    - user_data_source_impl.dart: `createUser` 메서드에서 Supabase의 `upsert` 기능을 사용하여 `UserDto` 데이터를 'users' 테이블에 저장하는 로직 구현.

--- lib/core/data/data_source/user/user_data_source_impl.dart
+++ lib/core/data/data_source/user/user_data_source_impl.dart
@@ -1,10 +1,17 @@
 import 'package:capstone_2026/core/data/data_source/user/user_data_source.dart';
 import 'package:capstone_2026/core/data/dto/user/user_dto.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class UserDataSourceImpl implements UserDataSource {
+  final SupabaseClient _supabaseClient;
+
+  UserDataSourceImpl({
+    required SupabaseClient supabaseClient,
+  }) : _supabaseClient = supabaseClient;
+
   @override
-  Future<void> createUser(UserDto userDto) {
-    // TODO: implement insertUser
-    throw UnimplementedError();
+  Future<void> createUser(UserDto userDto) async {
+    // TODO: 임시 구현, 에러 핸들링 등 추가해야됨.
+    await _supabaseClient.from('users').upsert(userDto.toJson());
   }
 }
