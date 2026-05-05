4bfbf9576166bf0a15b639e00a9d29d900e5a6ec
sesang <sesang7899@gmail.com>
2026-05-06 00:46:51 +0900

    feat: add mock review photo support

--- lib/feature/my_page/review_history/presentation/screen/review_history_screen.dart
+++ lib/feature/my_page/review_history/presentation/screen/review_history_screen.dart
@@ -607,6 +607,7 @@ class _ReviewEditBottomSheetState extends State<_ReviewEditBottomSheet> {
         rating: _rating,
         content: content,
         visitTag: visitPurpose.isEmpty ? null : visitPurpose,
+        imagePaths: widget.review.imageUrls,
       ),
     );
   }
