002368ca4fcf7a4e21bc4085102c4ab07573cece
Hagminism <iilhm110629@gmail.com>
2026-05-06 18:02:38 +0900

    feat: 요일 정보를 관리하는 WeekDay 열거형(Enum) 추가
    - lib/core/domain/model/enum/week_day.dart: 월요일부터 일요일까지의 요일 데이터를 정의하고, 각 요일에 대응하는 DB 키(`dbKey`)와 한글 라벨(`label`) 필드 구현.

---
+++ lib/core/domain/model/enum/week_day.dart
@@ -0,0 +1,14 @@
+enum WeekDay {
+  monday('monday', '월'),
+  tuesday('tuesday', '화'),
+  wednesday('wednesday', '수'),
+  thursday('thursday', '목'),
+  friday('friday', '금'),
+  saturday('saturday', '토'),
+  sunday('sunday', '일');
+
+  final String dbKey;
+  final String label;
+
+  const WeekDay(this.dbKey, this.label);
+}
