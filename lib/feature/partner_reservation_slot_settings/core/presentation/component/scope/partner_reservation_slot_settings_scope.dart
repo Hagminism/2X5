f544cf4ef44ec82fa5b64ff24cc70812b75d2995
Hagminism <iilhm110629@gmail.com>
2026-05-07 17:50:13 +0900

    feat: 파트너 예약 슬롯 설정 Scope 컴포넌트 추가
    - partner_reservation_slot_settings_scope.dart: `PartnerReservationSlotSettingsViewModel`을 주입받아 관리하고 `PartnerReservationSlotSettingsScreenRoot`로 전달하는 Scope 위젯 구현.

---
+++ lib/feature/partner_reservation_slot_settings/core/presentation/component/scope/partner_reservation_slot_settings_scope.dart
@@ -0,0 +1,26 @@
+import 'package:capstone_2026/feature/partner_reservation_slot_settings/presentation/screen/partner_reservation_slot_settings_screen_root.dart';
+import 'package:capstone_2026/feature/partner_reservation_slot_settings/presentation/screen/partner_reservation_slot_settings_view_model.dart';
+import 'package:flutter/material.dart';
+
+class PartnerReservationSlotSettingsScope extends StatefulWidget {
+  final PartnerReservationSlotSettingsViewModel viewModel;
+
+  const PartnerReservationSlotSettingsScope({
+    super.key,
+    required this.viewModel,
+  });
+
+  @override
+  State<PartnerReservationSlotSettingsScope> createState() =>
+      _PartnerReservationSlotSettingsScopeState();
+}
+
+class _PartnerReservationSlotSettingsScopeState
+    extends State<PartnerReservationSlotSettingsScope> {
+  late final PartnerReservationSlotSettingsViewModel viewModel = widget.viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return PartnerReservationSlotSettingsScreenRoot(viewModel: viewModel);
+  }
+}
