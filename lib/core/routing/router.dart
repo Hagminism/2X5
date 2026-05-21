740a7b9b7254db0b6b29ecabb126b11f938826b2
Hagminism <iilhm110629@gmail.com>
2026-05-21 14:55:36 +0900

    chore: 라우터 내 불필요한 name 속성 제거
    - router.dart: `Routes.homeStoreInformation` 경로에 정의된 불필요한 `name: 'information'` 속성을 삭제하여 코드 정리.

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -162,7 +162,6 @@ final router = GoRouter(
                   builder: (context, state) => const SearchScreen(),
                 ),
                 GoRoute(
-                  name: 'information',
                   parentNavigatorKey: _rootNavigatorKey,
                   path: Routes.homeStoreInformation,
                   builder: (context, state) {
